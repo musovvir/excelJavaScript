ac6a211910f09af3da465f01f77032897cc701a9
musovvir <mr.muzagov@mail.ru>
2023-10-19 00:25:36 +0300

    Finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,15 +3,18 @@ const CODES = {
   Z: 90,
 }
 
-function toCell(_, col) {
-  return `
-    <div
+function toCell(row) {
+  return function(_, col) {
+    return `
+      <div
       class="cell"
-      data-type="resizable"
       contenteditable
       data-col=${col}
+      data-type="cell"
+      data-id="${row}:${col}"
     ></div>
-  `
+    `
+  }
 }
 
 function toCol(col, index) {
@@ -55,13 +58,13 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  const cells = new Array(colsCount)
-      .fill('')
-      .map(toCell)
-      .join('')
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell(row))
+        .join('')
 
-  for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(row + 1, cells))
   }
 
   return rows.join('')
